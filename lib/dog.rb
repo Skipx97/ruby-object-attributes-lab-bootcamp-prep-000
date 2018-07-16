6b359ddf37552ff8048181e4836e6fc52167754c
Derrick Skipwith <dskipwith15@gmail.com>
2018-07-16 13:56:02 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+def  name=(full_name)
+     @name = full_name
+   end
+
+def name
+      @name
+   end
+
+   def breed=(breed)
+      @breed = breed
+    end
+
+    def breed
+      @breed
+    end
+  end
+
+    
